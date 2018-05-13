42016f6de145be71670180480a3de65c69e8dbf2
SeibelStan <seibel.stan@ya.ru>
2018-05-13 09:00:12 +0600

    Up

--- assets/home.js
+++
@@ -1,9 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://api.github.com/repos/SeibelStan/seibelstan.github.io/commits/master', true);
-request.onload = function() {
-    if(this.status == 200) {
-        var data = JSON.parse(this.response);
-        document.querySelector('.update').innerHTML = data.commit.author.date.split('T')[0];
-    }
-}
-request.send();
