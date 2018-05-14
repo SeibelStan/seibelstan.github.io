3d61f313e1f2433e4b8027680433e0aa2a331718
SeibelStan <seibel.stan@ya.ru>
2018-05-15 04:36:35 +0600

    Up

--- assets/home.js
+++ assets/home.js
@@ -18,36 +18,4 @@ window.onhashchange = function () {
     renav();
 }
 
-function scroll() {
-    var el = document.elementFromPoint(window.innerWidth / 2, window.innerHeight / 5);
-
-    if(!el) {
-        return false;
-    }
-
-    sect = el.closest('section');
-
-    var hash = sect.getAttribute('id');
-
-    if(
-        window.scrollY + window.innerHeight ==
-        q('html').clientHeight
-    ) {
-        hash = q('section:last-of-type').getAttribute('id');
-    }
-
-    if(history.pushState) {
-        history.pushState(null, null, '#' + hash);
-    }
-    else {
-        location.hash = hash;
-    }
-
-    renav();
-}
-
-document.addEventListener("touchmove", scroll, false);
-document.addEventListener("scroll", scroll, false);
-document.addEventListener("mousewheel", scroll, false);
-
 renav();
