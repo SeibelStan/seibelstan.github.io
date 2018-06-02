f391d215f64fb3f2f1ee95c19d36f49186b3843e
Seibel Stan <seibel.stan@ya.ru>
2018-06-02 18:48:04 +0600

    Up

--- assets/home.js
+++ assets/home.js
@@ -2,20 +2,19 @@ function q(s) {
     return document.querySelector(s);
 }
 
-function renav() {
-    if(!location.hash) {
+function qs(s) {
+    return document.querySelectorAll(s);
+}
+
+[].forEach.call(qs('.spoiler'), function (el) {
+    el.classList.add('hide');
+    el.innerHTML += '<a href="#" class="reveal">' + (el.getAttribute('data-reveal') || 'More') + '</a>';
+});
+
+[].forEach.call(qs('.reveal'), function (el) {
+    el.onclick = function () {
+        this.parentNode.classList.remove('hide');
+        this.remove();
         return false;
     }
-
-    var lastNaved = q('.naved');
-    if(lastNaved) {
-        lastNaved.classList.remove('naved');
-    }
-    q(location.hash).classList.add('naved');
-}
-
-window.onhashchange = function () {
-    renav();
-}
-
-renav();
+});
