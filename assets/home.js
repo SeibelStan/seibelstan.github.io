68d70e45a5588257ff4ee0ac78cb141e86d82666
Seibel Stan <seibel.stan@ya.ru>
2018-06-16 18:47:17 +0600

    Rus loc

--- assets/home.js
+++ assets/home.js
@@ -6,9 +6,13 @@ function qs(s) {
     return document.querySelectorAll(s);
 }
 
+function altlan(v1, v2) {
+    return location.pathname.match(/-*ru/) ? v2 : v1;
+}
+
 [].forEach.call(qs('.spoiler'), function (el) {
     el.classList.add('hide');
-    el.innerHTML += '<a href="#" class="more">' + (el.getAttribute('data-more') || 'More') + '</a>';
+    el.innerHTML += '<a href="#" class="more">' + (el.getAttribute('data-more') || altlan('More', 'Ещё')) + '</a>';
 });
 
 [].forEach.call(qs('.more'), function (el) {
